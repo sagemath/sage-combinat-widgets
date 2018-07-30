87658157d007a9ad35c87528eaedf60a674bbe21
Zerline <odile.benassy@u-psud.fr>
2018-07-30 16:57:53 +0200

    Just for passing tests. Nothing is notebook-tested.

--- sage_combinat_widgets/tableau_widget.py
+++ sage_combinat_widgets/tableau_widget.py
@@ -3,10 +3,9 @@ r"""
 An editable Young Tableau for Sage Jupyter Notebook
 
 EXAMPLES ::
-S = StandardTableaux(15)
-t = S.random_element()
-widget = TableauWidget(t)
-display(t)
+    sage: from sage_combinat_widgets import TableauWidget
+    sage: t = StandardTableaux(15).random_element()
+    sage: widget = TableauWidget(t)
 
 AUTHORS:
 - Odile Bénassy, Nicolas Thiéry
@@ -17,6 +16,7 @@ from sage.misc.bindable_class import BindableClass
 from sage.combinat.tableau import *
 from sage.all import SageObject
 from ipywidgets import Layout, VBox, HBox, Text, Label, HTML
+from .grid_widget import GridWidget
 import traitlets
 
 cell_layout = Layout(width='3em',height='2em', margin='0',padding='0')
@@ -35,23 +35,20 @@ css_lines.append(".pink { background-color: lightpink; }")
 css_lines.append(".yellow { background-color: lightyellow; }")
 css = HTML("<style>%s</style>" % '\n'.join(css_lines))
 
-ip = get_ipython()
-for base in ip.__class__.__mro__:
-    """If we are in a notebook, we will find 'notebook' in those names"""
-    if 'otebook' in base.__name__:
-        ip.display_formatter.format(css)
-        break
-
 try:
-    display(css)
+    ip = get_ipython()
+    for base in ip.__class__.__mro__:
+        """If we are in a notebook, we will find 'notebook' in those names"""
+        if 'otebook' in base.__name__:
+            ip.display_formatter.format(css)
+            break
 except:
-    pass # We are not in a notebook
-
+    pass # We are in the test environment
 
 class TCell(Text):
     r"""Single cell for a Young Tableau."""
 
-    def __init__(self, parent, coord, content):
+    def __init__(self, content, **kwargs):
         r"""
         Initializes a Tableau cell and compute its value.
 
@@ -64,12 +61,10 @@ class TCell(Text):
         TESTS::
 
         sage: from sage_combinat_widgets.tableau_widget import TCell
-        sage: c = TCell(None, (0,0), 5)
+        sage: c = TCell(5)
         sage: assert c.value == '5'
         """
-        super(TCell, self).__init__(layout=cell_layout, continuous_update=False)
-        self.parent = parent
-        self.coord = coord
+        super(TCell, self).__init__('', layout=cell_layout, continuous_update = False)
         self.content = content
         self.value = str(self.content)
         self.placeholder = str(content)
@@ -80,7 +75,7 @@ class TCell(Text):
         TESTS::
 
         sage: from sage_combinat_widgets.tableau_widget import TCell
-        sage: c = TCell(None, (0,0), 5)
+        sage: c = TCell(5)
         sage: from traitlets import Bunch
         sage: b = Bunch({'owner': c, 'new': u'10', 'old': u'5', 'name': 'value', 'type': 'change'})
         sage: assert b.new == '10'
@@ -91,35 +86,6 @@ class TCell(Text):
             return
         self.value = change.new
         self.content = int(self.value)
-
-import sage.misc.classcall_metaclass
-class MetaHasTraitsClasscallMetaclass (traitlets.traitlets.MetaHasTraits, sage.misc.classcall_metaclass.ClasscallMetaclass):
-    pass
-class BindableWidgetClass(BindableClass):
-    __metaclass__ = MetaHasTraitsClasscallMetaclass
-
-class GridWidget(VBox, BindableWidgetClass):
-    r"""Base Jupyter Interactive Widget for Sage Grid Objects
-
-    Composed of TCells.
-    """
-    value = traitlets.Instance(SageObject)
-
-    def __init__(self, obj):
-        r"""
-        TESTS::
-
-            sage: from sage_combinat_widgets import GridWidget
-            sage: import matrix, graphs
-            sage: from sage.graphs.generic_graph import GenericGraph
-            sage: m = matrix.identity(2)
-            sage: g = graphs.GridGraph(3)
-            sage: wm = GridWidget(m)
-            sage: wg = GridWidget(g)
-        """
-        super(GridWidget, self).__init__()
-        self.value = obj
-        self.cells = {}
 
 class TableauWidget(GridWidget):
     """Jupyter Widget for exploring a Young Tableau"""
@@ -135,18 +101,11 @@ class TableauWidget(GridWidget):
             sage: w.compute_status()
             sage: assert w.status == 'standard'
         """
-        super(TableauWidget, self).__init__(tbl)
+        super(TableauWidget, self).__init__(tbl, TCell)
         self.size = tbl.size()
         self.initial_lvalue = [[c for c in r] for r in self.value]
         self.output = Label()
         self.output.add_class('invisible')
-        self.cells = {}
-        for r in self.value:
-            for i in r:
-                coord = (self.value.index(r), r.index(i))
-                self.cells[coord] = TCell(self, coord, i)
-                self.add_traits(**{'cell_%d_%d' % (coord) : traitlets.Unicode()})
-                traitlets.link((self, 'cell_%d_%d' % (coord)), (self.cells[coord], 'value'))
         self.display_convention = display
         if self.display_convention == 'fr':
             rows = list(self.value)
@@ -212,7 +171,7 @@ class SemistandardTableauWidget(TableauWidget):
         r"""
         TESTS::
 
-        sage: from sage_combinat_widgets import *
+        sage: from sage_combinat_widgets import SemistandardTableauWidget
         sage: S = SemistandardTableaux(15)
         sage: t = S.random_element()
         sage: widget = SemistandardTableauWidget(t)
@@ -229,7 +188,7 @@ class StandardTableauWidget(TableauWidget):
         r"""
         TESTS::
 
-        sage: from sage_combinat_widgets import *
+        sage: from sage_combinat_widgets import StandardTableauWidget
         sage: S = StandardTableaux(15)
         sage: t = S.random_element()
         sage: widget = StandardTableauWidget(t)
@@ -251,7 +210,7 @@ class PartitionWidget(TableauWidget):
 
         TESTS::
 
-        sage: from sage_combinat_widgets import *
+        sage: from sage_combinat_widgets import PartitionWidget
         sage: from sage.combinat.partition import Partitions
         sage: p = Partitions(7)[5]
         sage: w = PartitionWidget(p)
