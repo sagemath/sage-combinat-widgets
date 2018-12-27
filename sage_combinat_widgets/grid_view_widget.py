207df2a069fcf66fb9989c562a6f547b784709a6
Zerline <odile.benassy@u-psud.fr>
2018-12-27 18:26:15 +0100

    Even more flexibility. Guess it'll be useful some time.

--- sage_combinat_widgets/grid_view_widget.py
+++ sage_combinat_widgets/grid_view_widget.py
@@ -188,7 +188,8 @@ class GridViewWidget(GridViewEditor, VBox, ValueWidget):
     r"""A widget for all grid-representable Sage objects
     """
 
-    def __init__(self, obj, adapter=None, display_convention='en', cell_layout=None, cell_widget_classes=[TextCell], \
+    def __init__(self, obj, adapter=None, display_convention='en', cell_layout=None, \
+                 cell_widget_classes=[TextCell], cell_widget_class_index=lambda x:0, \
                  blank_widget_class=BlankCell, addable_widget_class=AddableTextCell):
         r"""
         Grid View Widget initialization.
@@ -233,6 +234,7 @@ class GridViewWidget(GridViewEditor, VBox, ValueWidget):
                 cell_layout = textcell_layout
         self.cell_layout = cell_layout
         self.cell_widget_classes = cell_widget_classes
+        self.cell_widget_class_index = cell_widget_class_index
         self.displaytype = cell_widget_classes[0].displaytype
         self.cast = lambda x:self.adapter.display_to_cell(x, self.displaytype)
         self.blank_widget_class = blank_widget_class
@@ -310,33 +312,41 @@ class GridViewWidget(GridViewEditor, VBox, ValueWidget):
             if child and hasattr(child, 'value') and traitname in self.traits():
                 self.links.append(cdlink((child, 'value'), (self, traitname), self.cast))
 
-    def draw(self):
+    def draw(self, cell_widget_classes=None, cell_widget_class_index=None,
+             addable_widget_class=None, blank_widget_class=None):
         r"""
-        Add children to the Box:
-        - Sage object/gris editor cells
+        Add children to the GridWidget:
+        - Sage object/grid editor cells
         - Blank cells for empty cells in a row
         - Addable cells if any
+        Used classes can be passed as arguments
+        to enable changing shapes, colors ..
         """
         self.reset_links()
         positions = sorted(list(self.cells.keys()))
         rows = [[(pos, self.cells[pos]) for pos in positions if pos[0]==i] \
                 for i in uniq([t[0] for t in positions])]
         vbox_children = []
-        def cell_widget_class_index(pos):
-            # Such function can enable for example various shapes or colors
-            return 0
         addable_positions = self.addable_cells()
         addable_rows = []
         removable_positions = self.removable_cells()
         addable_rows = [(i,[pos for pos in addable_positions if pos[0]==i]) \
                         for i in uniq([t[0] for t in addable_positions])]
+        if not cell_widget_classes:
+            cell_widget_classes = self.cell_widget_classes
+        if not cell_widget_class_index:
+            cell_widget_class_index = self.cell_widget_class_index
+        if not addable_widget_class:
+            addable_widget_class = self.addable_widget_class
+        if not blank_widget_class:
+            blank_widget_class = self.blank_widget_class
         for i in range(len(rows)):
             j = 0
             hbox_children = []
             while j<=max([t[0][1] for t in rows[i]]):
                 if (i,j) in positions:
                     cell_content = self.cells[(i,j)]
-                    cell_widget_class = self.cell_widget_classes[cell_widget_class_index((i,j))]
+                    cell_widget_class = cell_widget_classes[cell_widget_class_index((i,j))]
                     cell_string = self.adapter.cell_to_display(cell_content, self.displaytype)
                     cell = cell_widget_class(cell_string,
                                              (i,j),
@@ -353,9 +363,9 @@ class GridViewWidget(GridViewEditor, VBox, ValueWidget):
                     hbox_children.append(cell)
                 elif (i,j) in addable_positions:
                     # Inside the grid-represented object limits
-                    hbox_children.append(self.addable_widget_class((i,j), self.cell_layout))
+                    hbox_children.append(addable_widget_class((i,j), self.cell_layout))
                 else:
-                    hbox_children.append(self.blank_widget_class(self.cell_layout))
+                    hbox_children.append(blank_widget_class(self.cell_layout))
                 j+=1
                 if j > max([t[0][1] for t in rows[i]]) and (i,j) in addable_positions:
                     # Outside of the grid-represented object limits
