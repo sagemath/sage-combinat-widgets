adeb767e3e89bb90f5e99bfcb8e21a246e29d02b
Zerline <odile.benassy@u-psud.fr>
2019-01-22 13:41:10 +0100

    Fully working version.

--- js/lib/embed.js
+++ js/lib/embed.js
@@ -5,5 +5,5 @@
 // already be loaded by the notebook otherwise.
 
 // Export widget models and views, and the npm package version number.
-module.exports = require('./TextWithTitle.js');
+module.exports = require('./TextWithTooltip.js');
 module.exports['version'] = require('../package.json').version;
--- js/lib/index.js
+++ js/lib/index.js
@@ -1,3 +1,3 @@
 // Export widget models and views, and the npm package version number.
-module.exports = require('./TextWithTitle.js');
+module.exports = require('./TextWithTooltip.js');
 module.exports['version'] = require('../package.json').version;
