6aa417e2ca67dd5dc3efd305f29748364deacab1
Zerline <odile.benassy@u-psud.fr>
2019-02-14 20:41:49 +0100

    Old sticking code in __init__.

--- sage_combinat_widgets/__init__.py
+++ sage_combinat_widgets/__init__.py
@@ -3,6 +3,5 @@ from __future__ import print_function, absolute_import
 from .grid_view_editor import GridViewEditor
 from .grid_view_widget import BlankCell, GridViewWidget
 from sage_widget_adapters import *
-from .example import HelloWorld
 #from .tableau_widget import TableauWidget, SemistandardTableauWidget, StandardTableauWidget, PartitionWidget
 #from .dominos import DominosWidget
