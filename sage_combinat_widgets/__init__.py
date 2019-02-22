4563f9cea4996bb05a585a79e96ac9960bcb4a9f
Zerline <odile.benassy@u-psud.fr>
2019-02-22 19:49:44 +0100

    Create a button for changing the widget value: a first working example.

--- sage_combinat_widgets/__init__.py
+++ sage_combinat_widgets/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import print_function, absolute_import
 # Add the import for which you want to give a direct access
 from .grid_view_editor import GridViewEditor
-from .grid_view_widget import BlankCell, GridViewWidget
+from .grid_view_widget import BlankCell, DisabledButtonCell, BlankButton, styled_button_cell, styled_push_button, GridViewWidget
 from sage_widget_adapters import *
-#from .tableau_widget import TableauWidget, SemistandardTableauWidget, StandardTableauWidget, PartitionWidget
-#from .dominos import DominosWidget
