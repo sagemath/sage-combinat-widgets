3352602059acdfa936239555d086c987037c8e4c
Zerline <odile.benassy@u-psud.fr>
2019-03-29 15:27:27 +0100

    Revert "Not jsdeps for egg_info." This line is needed for first installation.
    This reverts commit 6a7b8196880d30aaff2b8ccd6fd7072719634305.

--- setup.py
+++ setup.py
@@ -136,6 +136,10 @@ class NPM(Command):
         # update package data in case this created new files
         update_package_data(self.distribution)
 
+#version_ns = {}
+#with open(os.path.join(here, 'sage_combinat_widgets', '_version.py')) as f:
+#    exec(f.read(), {}, version_ns)
+
 setup_args = {
     'name': 'sage-combinat-widgets',
     'version': readfile("VERSION"),
@@ -171,6 +175,7 @@ setup_args = {
     'cmdclass': {
         'test': SageTest, # adding a special setup command for tests
         'build_py': js_prerelease(build_py),
+        'egg_info': js_prerelease(egg_info),
         'sdist': js_prerelease(sdist, strict=True),
         'jsdeps': NPM,
     },
