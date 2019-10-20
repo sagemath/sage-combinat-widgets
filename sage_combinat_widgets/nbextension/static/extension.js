c3fa42be336e1c0653429296b11879e3db688f55
Zerline <odile.benassy@u-psud.fr>
2019-10-20 21:26:01 +0200

    Had forgotten to git move 'extension.js'.

--- sage_combinat_widgets/nbextension/static/extension.js
+++ sage_combinat_widgets/nbextension/static/extension.js
@@ -11,22 +11,6 @@ define(function() {
         }
     });
 
-    var initialize = function () {
-        // update params with any specified in the server's config file.
-        // the "thisextension" value of the Jupyter notebook config's
-        // data may be undefined, but that's ok when using JQuery's extend
-        $.extend(true, params, Jupyter.notebook.config.thisextension);
-
-        // add our extension's css to the page
-        $('<link/>')
-            .attr({
-                rel: 'stylesheet',
-                type: 'text/css',
-                href: requirejs.toUrl('../style/sage-combinat-widgets.css')
-            })
-            .appendTo('head');
-    };
-
     // Export the required load_ipython_extension function
     return {
         load_ipython_extension : function() {}
