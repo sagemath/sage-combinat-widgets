5012594dbc34ede4e6ce2e51246cbdc655b1c361
Zerline <odile.benassy@u-psud.fr>
2019-10-21 11:51:02 +0200

    Support for parallelogram polyomino appears only in Sage 8.9, so do not crash if you have a later version.

--- sage_combinat_widgets/grid_view_editor.py
+++ sage_combinat_widgets/grid_view_editor.py
@@ -91,10 +91,13 @@ def get_adapter(obj):
     if issubclass(obj.__class__, SkewTableau):
         from sage_widget_adapters.combinat.skew_tableau_grid_view_adapter import SkewTableauGridViewAdapter
         return SkewTableauGridViewAdapter()
-    from sage.combinat.parallelogram_polyomino import ParallelogramPolyomino
-    if issubclass(obj.__class__, ParallelogramPolyomino):
-        from sage_widget_adapters.combinat.parallelogram_polyomino_grid_view_adapter import ParallelogramPolyominoGridViewAdapter
-        return ParallelogramPolyominoGridViewAdapter()
+    try:
+        from sage.combinat.parallelogram_polyomino import ParallelogramPolyomino
+        if issubclass(obj.__class__, ParallelogramPolyomino):
+            from sage_widget_adapters.combinat.parallelogram_polyomino_grid_view_adapter import ParallelogramPolyominoGridViewAdapter
+            return ParallelogramPolyominoGridViewAdapter()
+    except:
+        pass # support for parallelogram polyomino appears only in Sage 8.9
     from sage.matrix.matrix2 import Matrix
     if issubclass(obj.__class__, Matrix):
         from sage_widget_adapters.matrix.matrix_grid_view_adapter import MatrixGridViewAdapter
