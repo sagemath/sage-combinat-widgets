b711d518133acbafcf4729e893c46601c6d87bf4
Zerline <odile.benassy@u-psud.fr>
2019-10-22 14:58:48 +0200

    Added 'plugin.ts' for a valid Jupyterlab extension.

---
+++ js/src/plugin.ts
@@ -0,0 +1,46 @@
+// Copyright (c) Odile Bénassy, Nicolas Thiéry
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  Application, IPlugin
+} from '@phosphor/application';
+
+import {
+  Widget
+} from '@phosphor/widgets';
+
+import {
+  IJupyterWidgetRegistry
+ } from '@jupyter-widgets/base';
+
+import * as widgetExports from './singleton_widgets';
+
+import {
+  MODULE_NAME, MODULE_VERSION
+} from './version';
+
+const EXTENSION_ID = 'sage-combinat-widgets:plugin';
+
+/**
+ * The example plugin.
+ */
+const sageCombinatWidgetsPlugin: IPlugin<Application<Widget>, void> = {
+  id: EXTENSION_ID,
+  requires: [IJupyterWidgetRegistry],
+  activate: activateWidgetExtension,
+  autoStart: true
+};
+
+export default sageCombinatWidgetsPlugin;
+
+
+/**
+ * Activate the widget extension.
+ */
+function activateWidgetExtension(app: Application<Widget>, registry: IJupyterWidgetRegistry): void {
+  registry.registerWidget({
+    name: MODULE_NAME,
+    version: MODULE_VERSION,
+    exports: widgetExports,
+  });
+}
